7f0f375c22d66080b1154791e8d0e08f8f08076c
PONNEELAN-G <111427766+PONNEELAN-G@users.noreply.github.com>
2022-11-08 19:40:57 +0530

    Add files via upload

---
+++ Lab_3_Question_1/src/com/gl/javafsd/dsa/balancedbrackets/BalancedBracketsCheckerTest.java
@@ -0,0 +1,27 @@
+package com.gl.javafsd.dsa.balancedbrackets;
+
+public class BalancedBracketsCheckerTest {
+	
+	public static void main(String[] args) {
+		
+		test("([[{}]])");
+		
+		test("([[{}]]))");
+
+		
+	}
+
+
+	static void test(String brackets) {
+		
+		BalancedBracketsChecker checker = new BalancedBracketsChecker(
+			brackets);
+		
+		boolean result = checker.check();
+		if (result) {
+			System.out.println("The entered String " + brackets + " has Balanced Brackets");
+		}else {
+			System.out.println("The entered String " + brackets + " do not contain Balanced Brackets");			
+		}		
+	}
+}
